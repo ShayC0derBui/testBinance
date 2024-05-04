306a374595ef764b7ecf174d4300da53562032d7
ShayC0derBui <sinhaprem636@gmail.com>
2024-05-04 18:16:35 +0530

    init

---
+++ main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import base64
+import requests
+import time
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+
+# Set up authentication
+API_KEY='56GnIJPCqTPMOclemj1QnrD0VErS8cF4HEg2F9KcMkeopxUCfKQzf0OAVGoHMM1m'
+PRIVATE_KEY_PATH='test-prv-key.pem'
+
+# Load the private key.
+# In this example the key is expected to be stored without encryption,
+# but we recommend using a strong password for improved security.
+with open(PRIVATE_KEY_PATH, 'rb') as f:
+    private_key = load_pem_private_key(data=f.read(),
+                                       password=None)
+
+# Set up the request parameters
+params = {
+    'symbol':       'BTCUSDT',
+    'side':         'SELL',
+    'type':         'LIMIT',
+    'timeInForce':  'GTC',
+    'quantity':     '0.1',
+    'price':        '700',
+}
+
+# Timestamp the request
+timestamp = int(time.time() * 1000) # UNIX timestamp in milliseconds
+params['timestamp'] = timestamp
+
+# Sign the request
+payload = '&'.join([f'{param}={value}' for param, value in params.items()])
+signature = base64.b64encode(private_key.sign(payload.encode('ASCII')))
+params['signature'] = signature
+
+# Send the request
+headers = {
+    'X-MBX-APIKEY': API_KEY,
+}
+response = requests.post(
+    'https://api.binance.com/api/v3/order',
+    headers=headers,
+    data=params,
+)
+print(response.json())
